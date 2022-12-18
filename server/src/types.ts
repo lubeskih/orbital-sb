2efffc8a0e5024bdab26df12a74166292d6ba834
lubeskih <lubeskih@gmail.com>
2022-12-18 12:09:48 +0100

    moved shared types

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,19 +1,30 @@
 export interface Satellite {
-    name: string,
-    satnum: string,
-    inclination: number,
-    epoch_days: number,
-    latitude: number,
-    longitude: number,
-    orbital_period: number,
-    speed: number
-    tle_line_one: string,
-    tle_line_two: string,
-  }
-
+    name: string;
+    satnum: string;
+    inclination: number;
+    epoch_days: number;
+    latitude: number;
+    longitude: number;
+    orbital_period: number;
+    speed: number;
+    tle_line_one: string;
+    tle_line_two: string;
+}
 
 export interface TLE {
-  name: string,
-  line_1: string,
-  line_2: string,
+    name: string;
+    line_1: string;
+    line_2: string;
 }
+
+export type GroundTrackSlices = {
+    x: number;
+    y: number;
+    timeStamp: string;
+}[][];
+
+export type GroundTrack = {
+    x: number;
+    y: number;
+    timeStamp: string;
+}[];
